f227c2348f7a1cd70e9ef7994838964e18a683a7
Raúl Maese <rmaese@blacklabs.mx>
2019-07-01 20:03:47 -0500

    Commit inicial.

---
+++ app/src/main/java/mx/raulmaese/searchdialog/MainActivity.java
@@ -0,0 +1,67 @@
+package mx.raulmaese.searchdialog;
+
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.search_dialog);
+        dialog.show();
+
+        List<String> locationList =  Arrays.asList(getResources().getStringArray(R.array.stations));
+
+        listView = dialog.findViewById(R.id.lv_stations);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, locationList);
+
+        listView.setAdapter(arrayAdapter);
+
+        SearchView sch_station = dialog.findViewById(R.id.sch_station);
+        sch_station.setIconifiedByDefault(false);
+
+        sch_station.setIconifiedByDefault(false);
+        sch_station.setOnQueryTextListener(this);
+        sch_station.setSubmitButtonEnabled(true);
+        sch_station.setQueryHint("Search Here");
+
+        listView.setTextFilterEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            listView.clearTextFilter();
+        } else {
+            listView.setFilterText(newText);
+        }
+        return true;
+    }
+}
