8a73bacfff0c0ba816bf91221eb7bf79521425f4
Raúl Maese <rmaese@blacklabs.mx>
2019-07-01 23:10:15 -0500

    Correcciones.

--- app/src/main/java/mx/raulmaese/searchdialog/MainActivity.java
+++ app/src/main/java/mx/raulmaese/searchdialog/MainActivity.java
@@ -1,6 +1,8 @@
 package mx.raulmaese.searchdialog;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -28,27 +30,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final List<String> stationsList =  Arrays.asList(getResources().getStringArray(R.array.stations));
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                stationsList);
+
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setCancelable(false);
         dialog.setContentView(R.layout.search_dialog);
         dialog.show();
         dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
-                                    ViewGroup.LayoutParams.MATCH_PARENT);
+                ViewGroup.LayoutParams.MATCH_PARENT);
 
-        final List<String> stationsList =  Arrays.asList(getResources().getStringArray(R.array.stations));
-
-        listView = dialog.findViewById(R.id.lv_stations);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                stationsList);
-
-        listView.setAdapter(arrayAdapter);
-
-        SearchView sch_station = dialog.findViewById(R.id.sch_station);
+        final SearchView sch_station = dialog.findViewById(R.id.sch_station);
         sch_station.setIconifiedByDefault(false);
-        sch_station.setSubmitButtonEnabled(true);
         sch_station.setSuggestionsAdapter(null);
         filter = arrayAdapter.getFilter();
         sch_station.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -63,7 +61,33 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        listView = dialog.findViewById(R.id.lv_stations);
+        listView.setAdapter(arrayAdapter);
         listView.setTextFilterEnabled(false);
+
+        Button btn_show = findViewById(R.id.btn_show);
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.confirm_city)
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        Toast.makeText(MainActivity.this, sch_station.getQuery(), Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog confirmAlert = builder.create();
 
         Button btn_ok = dialog.findViewById(R.id.btn_ok);
         Button btn_cancelar = dialog.findViewById(R.id.btn_cancel);
@@ -78,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, stationsList.get(position).toString(), Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, arrayAdapter.getItem(position), Toast.LENGTH_LONG).show();
                 dialog.dismiss();
             }
         });
@@ -86,9 +110,15 @@ public class MainActivity extends AppCompatActivity {
         btn_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.dismiss();
+                if(arrayAdapter.isEmpty()){
+                    confirmAlert.show();
+                }else{
+                    dialog.dismiss();
+                }
             }
         });
+
+
 
     }
 
